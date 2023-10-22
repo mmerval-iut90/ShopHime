2462646379306fbfdeca4f2de45fb71cf37b6c3f
asoltner-iut90 <146073354+asoltner-iut90@users.noreply.github.com>
2023-10-22 15:43:30 +0200

    products

--- Audrick/shopPage/filters.js
+++ Audrick/shopPage/filters.js
@@ -12,12 +12,12 @@ function categorieFilters(){
     if(nchecked==0){
         for(var i = 0; i < products.length; i++){
             var product = products[i]
-            product.style.visibility = 'visible';
+            product.style.display = 'block';
         }
     }else{
         for(var i = 0; i < products.length; i++){
             var product = products[i]
-            product.style.visibility = 'hidden';
+            product.style.display = 'none';
         }
         for(var j = 0; j < categories.length; j++){
             var categorie = categories[j]
@@ -25,7 +25,7 @@ function categorieFilters(){
                 for(var i = 0; i < products.length; i++){
                     var product = products[i]
                     if(product.getElementsByClassName('productCategorie')[0].textContent == categorie.name){
-                        product.style.visibility = "visible";
+                        product.style.display = 'block';
                     }
                 }
             }
