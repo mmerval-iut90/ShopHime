36607407c9f309dfac0902dbecd4483ae05758dd
Quentin-BELHADJ <146366450+Quentin-BELHADJ@users.noreply.github.com>
2023-10-27 00:04:20 +0200

    Audrick a demandé de push

---
+++ ShopPages/One Piece/productsList.js
@@ -0,0 +1,18 @@
+products = [
+    ['Images/catakuri.jpg','One Piece - T-Shirt Cat-akuri', '69,0', 'Clothes'],
+    ['Images/dogflamingo.png', 'One Piece - T-Shirt Dog-flamingo', '69,0', 'Clothes'],
+    ['Images/bears.jpg', 'One Piece - T-Shirt Bears', '69,0', 'Clothes'],
+    ['Images/luffyWanted.webp', 'One Piece - Affiche Wanted Luffy', '9,99', 'Poster'],
+    ['Images/ChopperWanted.webp', 'One Piece - Affiche Wanted Chopper', '9,99', 'Poster'],
+    ['Images/usoppwanted.webp', 'One Piece - Affiche Wanted Usopp', '9,99', 'Poster'],
+    ['Images/CarrotFigure.jpg', 'One Piece - Figurine Carrot', '39,90', 'Figurine'],
+    ['Images/LuffyFigure.jpg', 'One Piece - Figurine Luffy Gear 5 ', '200,0', 'Figurine'],
+    ['Images/KaidoFigure.jpg', 'One Piece - Figurine Kaido', '250,0', 'Figurine'],
+    ['Images/doflamingofigure.jpg', 'One Piece - Figurine Doflamingo', '199,90', 'Figurine'],
+    ['Images/luffymp.webp', 'One Piece - Tapis de souris luffy Gear 5', '24,90', 'Mousepad'],
+    ['Images/MugiwaraMP.jpg', 'One Piece - Tapis de souris équipage du chapeau de paille', '25,0', 'Mousepad'],
+    ['Images/CopyGomu.jpg', 'One Piece - Figurine Lumineuse fruit du Gomu-Gomu', '14,90', 'Figurine'],
+    ['Images/luffykeychain.jpg', 'One Piece - Porte-clé Luffy ', '3,99', 'Keychain'],
+    ['Images/sanjikeychain.webp', 'One Piece - Porte-clé Sanji ', '2,99', 'Keychain'],
+    ['Images/chopperKeychain.jpg', 'One Piece - Porte-clé Chopper ', '2,99', 'Keychain']
+]
