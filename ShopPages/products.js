67067f16b26fb59ce2f1988e0dddaff8efa12794
asoltner-iut90 <146073354+asoltner-iut90@users.noreply.github.com>
2023-10-26 21:04:33 +0200

    update

--- ShopPages/products.js
+++ ShopPages/products.js
@@ -4,7 +4,7 @@ for (var i = 0; i < products.length; i++) {
     code += "<div class='productImage' style='background-image:"+ 'url("'+product[0]+'")'+";'></div>"
     code += "<p class='productName'>"+product[1]+"</p>"
     code += "<div class='price'><p class='productPrice'>"+product[2]+"</p><p class='unite'>$</p></div>"
-    code += "<p class='productCategorie'>"+product[3]+"</p>"
+    code += "<p class='productCategorie' hidden>"+product[3]+"</p>"
     code += "</div>"
     document.getElementById("ProductGrid").innerHTML += code;
 }
