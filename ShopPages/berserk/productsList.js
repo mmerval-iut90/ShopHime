872b88c900b4c24f2a4c68010c81cce8b9ca241c
mmerval-iut90 <146182043+mmerval-iut90@users.noreply.github.com>
2023-10-27 13:52:51 +0200

    Add files via upload

---
+++ ShopPages/berserk/productsList.js
@@ -0,0 +1,27 @@
+products = [
+    ['images/berserker_armor.jpg','Figurine armure du berserker', '80', 'figurine'],
+    ['images/griffith.jpg', 'Statue Griffith Legacy Art Bonus Version', '1090,90', 'figurine'],
+    ['images/skullknightAndhorse.jpg','Statue Skullknight', '799,0', 'figurine'],
+    ['images/skull knight.jpg','Figurine Skullknight', '359,90', 'figurine'],
+    ['images/zodd.jpg',"Statue Zodd l'immortel", '599,90', 'figurine'],
+    ['images/zodd monstre.jpg','Figurine Zodd released', '79,90', 'figurine'],
+
+    ['images/tome1.jpg','Tome 1-10 manga Berserk', '79,99', 'manga'],
+    ['images/tome11.jpg','Tome 11-20 manga Berserk', '79,99', 'manga'],
+    ['images/tome21.jpg','Tome 21-30 manga Berserk', '79,99', 'manga'],
+    ['images/tome31.jpg','Tome 31-40 manga Berserk', '79,99', 'manga'],
+    ['images/tome41.jpg','Tome 41 manga Berserk', '7,99', 'manga'],
+
+    ['images/poster.jpg','Poster eclispe PLAQUE METALIQUE', '44.50', 'poster'],
+
+    ['images/keychain_dragon_slayer.jpeg','Porte clé Dragonslayer', '4,99', 'porte clés'],
+
+    ['images/collier_marque_sacrifice.jpg','Collier marque du sacrifice', '9,99', 'bijoux'],
+    ['images/earring marque sacrifice.jpg',"Boucle d'oreille Marque du sacrifice", '14,99', 'bijoux'],
+
+    ['images/book holder.jpg','Book holder Berserk', '29,99', 'accessoires'],
+
+
+
+
+]
